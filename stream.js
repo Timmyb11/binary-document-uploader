f114c4452571f3f1150d0d17a7f3c51b632427bc
Amin Marashi <marashi.s.amin@gmail.com>
2017-08-11 15:50:03 +0800

    Add size and checksum checking

--- stream.js
+++ stream.js
@@ -2,35 +2,50 @@
 
 const WebSocket = require("ws");
 const { generateBinary } = require("./tools");
-const { sendAuthReq, uploadBinary, getFile } = require('./client');
+const { sendAuthReq, uploadBinary, getFile } = require("./client");
+const crypto = require("crypto");
+const shasum = crypto.createHash("sha1");
 
-const FILE_SIZE = process.env.FILE_SIZE || 1000000;
+const FILE_SIZE = Number(process.env.FILE_SIZE || 1000000);
 
 const ws = new WebSocket("ws://localhost:8080");
 
 const binaryData = generateBinary(FILE_SIZE);
 
+shasum.update(binaryData);
+
+const checksum = shasum.digest('hex');
+
 function onMessage(data) {
-    console.log(data);
-    const json = JSON.parse(data);
+  console.log(data);
+  const json = JSON.parse(data);
 
-    if (json.error) {
-        return console.log(json.error);
-    }
+  if (json.error) {
+    return console.log(json.error);
+  }
 
-    const metadata = getFile(json)
+  const metadata = getFile(json);
 
-    const {checksum} = metadata;
-    if (checksum) {
-        return;
-    }
-    uploadBinary(ws, metadata, binaryData)
+  const { checksum: receivedChecksum, size } = metadata;
+  if (receivedChecksum) {
+      if (receivedChecksum === checksum) {
+          console.log('Checksum matches');
+      } else {
+          console.log('Checksum does not match');
+      }
+      if (size === FILE_SIZE) {
+          console.log('Size matches');
+      } else {
+          console.log('Size does not match');
+      }
+    return;
+  }
+  uploadBinary(ws, metadata, binaryData);
 }
 
 function onOpen() {
-    sendAuthReq(ws)
+  sendAuthReq(ws);
 }
 
 ws.on("open", onOpen);
-ws.on('message', onMessage);
-
+ws.on("message", onMessage);
